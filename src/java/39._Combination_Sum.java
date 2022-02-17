859357e1eebdd12f210372ff3aa7b0c1ab9ee79f
JayTeli <kooljay999@gmail.com>
2022-02-17 22:10:41 +0530

    changes to problem 39

--- src/java/39._Combination_Sum.java
+++ src/java/39._Combination_Sum.java
@@ -1,4 +1,3 @@
-https://leetcode.com/problems/combination-sum/
 // Algo 1 : Backtracking : O(n ^(1 + (t/cmin))) , O(t/cmin) ... t = target, cmin = min value in candidate array 
 class Solution {
     private List<List<Integer>> result;
@@ -15,14 +14,15 @@ class Solution {
         this.result = new ArrayList<>();
     }
     private void backtrack(int remainingSum , LinkedList<Integer> combination , int startIndex) {
-        if(remainingSum == 0) result.add(new ArrayList<>(combination)); // Note : added deep copy of linkedlist instead of original list
-        else if(remainingSum < 0) return; // Stop exploration
-        
+        if(remainingSum < 0) return; // Stop exploration
+        else if(remainingSum == 0) {
+            result.add(new ArrayList<>(combination)); // Note : added deep copy of linkedlist instead of original list
+            return; // Note
+        }
         for(int i = startIndex ; i < size ; i++) {
             combination.add(candidates[i]);  
             backtrack(remainingSum - candidates[i] , combination , i);
             combination.removeLast();   // Backtracking
         }
-        
     }
 }
