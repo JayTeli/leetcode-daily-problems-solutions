dafc7c8b0e0f8c5a35f077cde298720439cca70b
Jay Teli <kooljay999@gmail.com>
2023-04-13 03:07:35 +0530

    Update 217._Contains_Duplicate.java

--- src/java/217._Contains_Duplicate.java
+++ src/java/217._Contains_Duplicate.java
@@ -3,18 +3,22 @@ https://leetcode.com/problems/contains-duplicate
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        for(int i = 1 ; i < nums.length ; i++) { // starting with i=1 as we have to check for i-1
-            if(nums[i-1] == nums[i]) return true;
+        for(int i = 0 ; (i+1) < nums.length ; i++) {
+            if(nums[i] == nums[i+1])
+                return true;
         }
         return false;
     }
 }
 // Algo 2 : Using HashSet : O(n) , O(n)
-public boolean containsDuplicate(int[] nums) {
-    Set<Integer> set = new HashSet<>(nums.length);
-    for (int x: nums) {
-        if (set.contains(x)) return true;
-        set.add(x);
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> visited = new HashSet<>();
+        for(int i : nums) {
+            if(visited.contains(i))
+                return true;
+            visited.add(i);
+        }
+        return false;
     }
-    return false;
 }
